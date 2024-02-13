cde4aefbe178b4fa5e7a2fff237849156dc31793
parvejme24 <mdparvejme24@gmail.com>
2024-02-13 19:10:13 +0600

    added add publisher controller

--- src/Controller/PublisherController/PublisherController.js
+++ src/Controller/PublisherController/PublisherController.js
@@ -23,3 +23,21 @@ exports.getPublisherById = async (req, res) => {
     res.status(500).josn({ error: err.message });
   }
 };
+
+// add a publisher
+exports.addPublisher = async (req, res) => {
+  try {
+    const updatedPublisher = await publisher.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!updatedPublisher) {
+      res.status(404).json({ error: "Publisher not found" });
+    } else {
+      res.json(updatedPublisher);
+    }
+  } catch (error) {
+    res.status(400).json({ error: err.message });
+  }
+};
