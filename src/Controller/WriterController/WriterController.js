26fa5c4abc7faa03536ca17ac17ebd0f83f5770f
parvejme24 <mdparvejme24@gmail.com>
2024-02-13 14:17:30 +0600

    added post writer controller

--- src/Controller/WriterController/WriterController.js
+++ src/Controller/WriterController/WriterController.js
@@ -1,9 +1,9 @@
-const writer = require("../../Models/writer/writer");
+const Writers = require("../../Models/writer/writer");
 
 // get all writer
 exports.getAllWriter = async (req, res) => {
   try {
-    const writers = await writer.find();
+    const writers = await Writers.find();
     res.json(writers);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -13,13 +13,24 @@ exports.getAllWriter = async (req, res) => {
 // get a writer by id
 exports.getWriterById = async (req, res) => {
   try {
-    const writer = await writer.findById(req.params.id);
-    if (!writer) {
+    const Writers = await Writers.findById(req.params.id);
+    if (!Writers) {
       res.status(404).json({ error: "writer not found" });
     } else {
-      res.json(writer);
+      res.json(Writers);
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+};
+
+// add a writer
+exports.addWriter = async (req, res) => {
+  const newWriter = new Writers(req.body);
+  try {
+    const saveWriter = await newWriter.save();
+    res.status(201).json(saveWriter);
+  } catch (error) {
+    res.status(500).json({ error: err.message });
   }
 };
