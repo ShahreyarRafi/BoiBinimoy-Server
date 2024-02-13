9535b5fd1fe843f1b7dbd5eba08ecc90d9d4a255
parvejme24 <mdparvejme24@gmail.com>
2024-02-13 19:06:14 +0600

    added get a publisher controller

--- src/Controller/PublisherController/PublisherController.js
+++ src/Controller/PublisherController/PublisherController.js
@@ -1,10 +1,25 @@
 const publisher = require("../../Models/publisher/publisher");
 
+// get all publisher
 exports.getAllPublisher = async (req, res) => {
   try {
     const allPublisher = await publisher.find();
     res.json(allPublisher);
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+};
+
+// get a publisher
+exports.getPublisherById = async (req, res) => {
+  try {
+    const aPublisher = await publisher.findById(req.params.id);
+    if (!aPublisher) {
+      res.status(404).json({ error: "Publisher not found" });
+    } else {
+      res.jso(aPublisher);
+    }
+  } catch (error) {
+    res.status(500).josn({ error: err.message });
   }
 };
