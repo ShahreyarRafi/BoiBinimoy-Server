30daa10d10ee523207428f28089bcee9aed2116b
taiatulislam <taiatulislamapon@gmail.com>
2024-02-21 16:44:02 +0600

    exchange book and request api added

--- src/app.js
+++ src/app.js
@@ -29,6 +29,8 @@ const jwtRoute = require("./Routes/jwt/jwtRoute");
 
 const bannerRouter = require("./Routes/BannerRoutes/BannerRoutes");
 const usersRoute = require("./Routes/Users/usersRoutes");
+const exchangeBooksRouter = require("./Routes/ExchangeBooksRoutes/ExchangeBooksRoutes");
+const requestBooksRouter = require("./Routes/RequestBooks/RequestBooks");
 const buyBookRouter = require("./Routes/BuyBooks/BuyBooksRoutes");
 const reviewsRouter = require("./Routes/ReviewsRoutes/ReviewsRoutes");
 const blogsRouter = require("./Routes/BlogsRoute/BlogsRoute");
@@ -67,6 +69,12 @@ app.use("/api/v1", bannerRouter);
 
 // users related api
 app.use("/api/v1", usersRoute);
+
+// exchange books related apis
+app.use("/api/v1", exchangeBooksRouter);
+
+// request book exchange related apis
+app.use("/api/v1", requestBooksRouter);
 
 // buy book related apis
 app.use("/api/v1", buyBookRouter);
