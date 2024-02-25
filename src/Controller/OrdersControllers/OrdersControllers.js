497cfe064f66e903b58407b6874f0366f457417f
Atik Sahariyar <atiksahariyar88@gmail.com>
2024-02-25 16:19:29 +0600

    updated book exchange model

--- src/Controller/OrdersControllers/OrdersControllers.js
+++ src/Controller/OrdersControllers/OrdersControllers.js
@@ -1,19 +1,68 @@
 const Orders = require("../../Models/Orders/Orders");
+const BuyBooks = require("../../Models/buyBooks/buyBooks");
 
+// get my orders by email
 exports.getMyOrders = async (req, res) => {
   try {
     const email = req.params.email;
     const filter = { clientEmail: email };
     const orders = await Orders.find(filter);
     const allCarts = [];
+    
     orders.forEach((order) => {
       const cartsInOrder = order.carts;
       allCarts.push(...cartsInOrder);
     });
+  
+    const bookPromises = allCarts.map(async (cart) => {
+      const id = cart?.book_id || "";
+      const book = await BuyBooks.findById(id) || {};
+      return book;
+    });
 
-    res.send({ orders, allCarts});
+    const books = await Promise.all(bookPromises);
+    
+    const myOrders = allCarts.map((cart, index) => {
+      return {
+        cart,
+        book: books[index] || "book not found"
+      }
+    }) 
+    
+
+    res.send({ orders, myOrders});
   } catch (error) {
     console.error("Error getting my orders data:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+
+// get seller orders by owner email;
+
+exports.getSellerOrders = async (req, res) => {
+  try{
+    const ownerEmail = req.params.email;
+    const orders = await Orders.find();
+    const allCarts = [];
+    
+    orders.forEach((order) => {
+      const cartsInOrder = order.carts;
+      allCarts.push(...cartsInOrder);
+    });
+  
+    const bookPromises = allCarts.map(async (cart) => {
+      const id = cart?.book_id;
+      const book = await BuyBooks.findById(id) || {};
+      return book;
+    });
+       
+    const books = await Promise.all(bookPromises);
+    const sellerBooks = books.filter((book) => book.owner_email === ownerEmail)
+   
+    res.send(sellerBooks)
+  }catch (error) {
+    console.error("Error getting my orders data:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
--- src/Routes/OrdersRouter/OrdersRouter.js
+++ src/Routes/OrdersRouter/OrdersRouter.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const { getMyOrders } = require("../../Controller/OrdersControllers/OrdersControllers");
+const { getMyOrders, getSellerOrders } = require("../../Controller/OrdersControllers/OrdersControllers");
 
 const OrdersRouter = express.Router();
 
 
 // get my orders by email
 OrdersRouter.get("/my-orders/:email", getMyOrders);
+
+// get my orders by email
+OrdersRouter.get("/seller-orders/:email", getSellerOrders);
 
 
 module.exports = OrdersRouter;
