22af76cf373832c6392d5c08e7adb8695b454e85
parvejme24 <mdparvejme24@gmail.com>
2024-03-07 00:16:29 +0600

    added total sell controller and router

--- src/Controller/AdminDashboardController/AdminDashboardController.js
+++ src/Controller/AdminDashboardController/AdminDashboardController.js
@@ -1,4 +1,5 @@
 const Orders = require("../../Models/Orders/Orders");
+const BuyBooks = require("../../Models/buyBooks/buyBooks");
 
 // get total orders
 exports.getTotalOrders = async (req, res) => {
@@ -7,6 +8,30 @@ exports.getTotalOrders = async (req, res) => {
     res.send({ totalOrders });
   } catch (error) {
     console.error("Error getting total orders data:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// get total sales
+exports.getTotalSales = async (req, res) => {
+  try {
+    const orders = await Orders.find();
+    let totalSales = 0;
+
+    for (const order of orders) {
+      for (const cart of order.carts) {
+        const book = await BuyBooks.findById(cart.book_id);
+        if (book) {
+          totalSales += book.price * cart.quantity;
+        } else {
+          console.error(`Book not found for cart with ID: ${cart.book_id}`);
+        }
+      }
+    }
+
+    res.send({ totalSales });
+  } catch (error) {
+    console.error("Error getting total sales:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
--- src/Routes/AdminDashboardRoutes/AdminDashboardRoutes.js
+++ src/Routes/AdminDashboardRoutes/AdminDashboardRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const {
   getTotalOrders,
+  getTotalSales,
 } = require("../../Controller/AdminDashboardController/AdminDashboardController");
 
 const adminDashboardRouters = express.Router();
 
-// get total order ruter
+// get total order router
 adminDashboardRouters.get("/total-orders", getTotalOrders);
+
+// get total sells router
+adminDashboardRouters.get("/total-sales", getTotalSales);
 
 module.exports = adminDashboardRouters;
